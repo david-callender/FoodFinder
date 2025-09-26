5e6b8def0c7a7f5c9e86faeab34c94c66e2af8eb
Riley Motylinski <riley@motylinski.com>
2025-09-26 14:16:27 -0500

    feat: login page + component

---
+++ frontend/src/components/LoginBox/LoginBox.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+// would it make more sense to keep components in respective page directory?
+
+export const LoginBox: FC = () => {
+  return (
+    <div>
+      <h1>This be a test</h1>
+    </div>
+  );
+};
