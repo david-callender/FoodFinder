30a466df101981d131eb7ed3e33a788ccf372241
Riley Motylinski <riley@motylinski.com>
2025-09-26 18:42:14 -0500

    added text input boxes

--- frontend/src/components/LoginBox/LoginBox.tsx
+++ frontend/src/components/LoginBox/LoginBox.tsx
@@ -3,9 +3,13 @@ import type { FC } from "react";
 // would it make more sense to keep components in respective page directory?
 
 export const LoginBox: FC = () => {
+  const textInputClass =
+    "bg-gray-200 w-40 place-self-center border-4 border-transparent text-black m-2";
   return (
-    <div>
-      <h1>This be a test</h1>
+    <div className="grid border-4 text-black">
+      <input type="text" className={textInputClass} />
+      <input type="text" className={textInputClass} />
+      <button>login</button>
     </div>
   );
 };
