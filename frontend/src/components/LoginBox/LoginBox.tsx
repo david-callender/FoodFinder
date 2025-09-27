203f57d171a78ee137216cef6d72a380b395b6b3
Riley Motylinski <riley@motylinski.com>
2025-09-27 13:45:17 -0500

    feat: added simple auth integration with backend

--- frontend/src/components/LoginBox/LoginBox.tsx
+++ frontend/src/components/LoginBox/LoginBox.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 
 import type { FC, FormEvent } from "react";
+import { json } from "stream/consumers";
 
 export const LoginBox: FC = () => {
   // Description: email/password text field and login button w/ redirect
@@ -12,19 +13,45 @@ export const LoginBox: FC = () => {
   // use for redirects
   const router = useRouter();
 
-  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     // prevents refresh of page
     event.preventDefault();
 
-    // for use later
-    // const formData = new FormData(event.currentTarget);
-    // const email = formData.get('email');
-    // const password = formData.get('password');
+    // pulling form data
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
 
-    // handle authentication here
+    // TODO: input validation
+
+    // handling authentication
+
+    // 9/27: endpoint currently assumes all users are valid.
+    // At some point, when validation is implemented,
+    // update this to a redirect (?) for registration
+    // if login fails
+
+    // how to get refresh token?
+
+    // FIX: Static URL
+    // request to login endpoint
+    const response = await fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: email, password: password }),
+    })
 
     // redirect to home page
-    router.push("/");
+    if (response.ok){
+     
+        let userInfo = await response.json();
+        // storing access token
+        localStorage.setItem("access_token", userInfo.access_token);
+        router.push("/");
+    } else {
+        console.log(await response.json())
+    }
+   
   }
 
   return (
