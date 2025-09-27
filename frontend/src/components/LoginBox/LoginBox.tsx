458578b24757627108bb1664935f09aa02ef69c9
Riley Motylinski <riley@motylinski.com>
2025-09-26 21:42:48 -0500

    feat: modified login to form

--- frontend/src/components/LoginBox/LoginBox.tsx
+++ frontend/src/components/LoginBox/LoginBox.tsx
@@ -1,15 +1,33 @@
+"use client";
+
 import type { FC } from "react";
 
 // would it make more sense to keep components in respective page directory?
 
 export const LoginBox: FC = () => {
   const textInputClass =
-    "bg-gray-200 w-40 place-self-center border-4 border-transparent text-black m-2";
+    "bg-gray-200 place-self-center border-4 p-0.5 m-2 rounded-lg text-black";
   return (
-    <div className="grid border-4 text-black">
-      <input type="text" className={textInputClass} />
-      <input type="text" className={textInputClass} />
-      <button>login</button>
-    </div>
+    <form>
+      <div className="m-20 flex flex-auto flex-col p-5">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className={textInputClass}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className={textInputClass}
+          required
+        />
+        <button className="mx-auto w-40 bg-gray-200 text-black hover:bg-gray-300">
+          login
+        </button>
+      </div>
+    </form>
   );
 };
