f8074e5426d4f5edf42dd58d9ce69845d9a541a8
Riley Motylinski <riley@motylinski.com>
2025-09-28 10:15:11 -0500

    docs: documentation for middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -4,20 +4,29 @@ import type { NextRequest } from "next/server";
 
 // make sure they have some key.
 // real authentication will happen within the api routes
-function authenticate(access_token: string | undefined): boolean {
-  if (access_token === undefined || access_token === "") {
+function authenticate(refresh_token: string | undefined): boolean {
+  // Description: shallow authentication to ensure a user has a refresh token
+  // args: refresh_token -- users refresh token
+  // returns: boolean : false if lacking refresh token, true otherwise
+
+  if (refresh_token === undefined || refresh_token === "") {
     return false;
   }
   return true;
 }
 
 export function middleware(request: NextRequest): NextResponse | undefined {
+  // Description:
+  // args: request: incoming request
+  // returns: NextResponse | undefined : either a redirect or allows the response through, unmodified
+
   // verifys if you have a token
   const authenticated = authenticate(
     request.cookies.get("refresh_token")?.value
   );
 
   if (!authenticated) {
+    // push user back to login page
     return NextResponse.redirect(new URL("/login", request.url));
   }
 }
