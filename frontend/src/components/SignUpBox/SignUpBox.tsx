5dbc1c845135441018474f65424e3fe0d00ac5b6
Riley Motylinski <riley@motylinski.com>
2025-09-30 16:49:41 -0500

    feat: aded submit + redirect logic

--- frontend/src/components/SignUpBox/SignUpBox.tsx
+++ frontend/src/components/SignUpBox/SignUpBox.tsx
@@ -1,8 +1,49 @@
-import type { FC } from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import type { FC, FormEvent } from "react";
 
 export const SignUpBox: FC = () => {
+  const router = useRouter();
+
+  async function handleSubmit(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    // prevents refresh of page
+    event.preventDefault();
+
+    // pulling form data
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const phoneNumber = formData.get("phone-number");
+
+    // TODO: accept phoneNumber at /register endpoint
+    console.log(phoneNumber);
+    // TODO: input validation
+
+    // request to login endpoint
+    // refresh_token cookie is set here
+    const response = await fetch(
+      new URL("/register", process.env.NEXT_PUBLIC_BACKEND_URL),
+      {
+        method: "POST",
+        credentials: "include", // need this for receive cookies w/ cors
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: email, password: password }),
+      }
+    );
+
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.log(response.status);
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="flex flex-col">
         <input
           type="email"
@@ -22,7 +63,8 @@ export const SignUpBox: FC = () => {
           type="tel"
           className="m-2 place-self-center rounded-lg border-4 bg-gray-200 p-0.5 text-black"
           name="phone-number"
-          placeholder="Phone Number"
+          placeholder="XXX-XXX-XXXX"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
         />
         <button className="mx-auto w-40 bg-gray-200 text-black hover:bg-gray-300">
           Sign Up!
