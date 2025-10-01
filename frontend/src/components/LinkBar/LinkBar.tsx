8c0d224b70cf567de6979e52412e3640fb00a8b8
Riley Motylinski <riley@motylinski.com>
2025-10-01 18:26:24 -0500

    feat: centered login/signup page links on /

--- frontend/src/components/LinkBar/LinkBar.tsx
+++ frontend/src/components/LinkBar/LinkBar.tsx
@@ -2,11 +2,11 @@ import type { FC } from "react";
 
 export const LinkBar: FC = () => {
   return (
-    <div className="m-2 mx-100 flex flex-row">
-      <div className="basis-1/2 text-center">
+    <div className="m-2 flex flex-row">
+      <div className="text-center grow">
         <a href="/login">login</a>
       </div>
-      <div className="basis-1/2 text-center">
+      <div className="text-center grow">
         <a href="/signup">signup</a>
       </div>
     </div>
