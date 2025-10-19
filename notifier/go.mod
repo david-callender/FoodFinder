9aa32de326d1def726de8d5ae9de4f927f8f8721
Tristan Anderson <and11342@umn.edu>
2025-10-19 12:40:35 -0500

    Initial commit starting work on a notifier to email users who have food preferences matching entries in the cache.

---
+++ notifier/go.mod
@@ -0,0 +1,3 @@
+module github.com/david-callender/FoodFinder/notifier
+
+go 1.25.1
---
+++ notifier/notifier.go
@@ -0,0 +1,11 @@
+package notifier
+
+import (
+	"github.com/jackc/pgx/v5"
+	"os"
+	"time"
+)
+
+func main() {
+
+}
