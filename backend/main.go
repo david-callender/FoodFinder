bb6858d8bcc02511ab438410d5d5acabbec72ce5
david-callender <calle159@umn.edu>
2025-10-20 20:08:29 -0500

    tidy up main.go

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/jackc/pgx/v5"
@@ -20,31 +21,32 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const ACCESS_TOKEN_KEEPALIVE = time.Minute * 7
+const REFRESH_TOKEN_KEEPALIVE = time.Hour * 24 * 10
+
 // user table in the db
-type Users struct {
-	ID          string
+type User struct {
+	ID          int
 	Email       string
-	Password    string
+	Password    []byte
 	DisplayName string
 }
 
-// GLOABL VAR STORAGE
+// GLOBAL VAR STORAGE
 type Server struct {
 	DB *pgxpool.Pool
 }
 
-const ACCESS_TOKEN_KEEPALIVE = time.Minute * 7
-const REFRESH_TOKEN_KEEPALIVE = time.Hour * 24 * 10
-
 var ErrEmailInUse = errors.New("email already in use")
 
+// Connects to the db and returns a connection pool.
 func connectDB() (*pgxpool.Pool, error) {
-	dsn := os.Getenv("connection_string")
-	if dsn == "" {
-		return nil, fmt.Errorf("DATABASE_URL (or db_url) is not set")
+	conStr := os.Getenv("DATABASE_URL")
+	if conStr == "" {
+		return nil, fmt.Errorf("environment variable 'DATABASE_URL' is not set")
 	}
 
-	db, err := pgxpool.New(context.Background(), dsn)
+	db, err := pgxpool.New(context.Background(), conStr)
 	if err != nil {
 		fmt.Println("failed to connect to database", err)
 		return db, err
@@ -58,18 +60,12 @@ func connectDB() (*pgxpool.Pool, error) {
 	return db, nil
 }
 
-// Purpose: check if email is exist in users table
-// Arguments: db: *sql.DB (sql database model),
-//			  email: string (email of user)
-// Return: exist: boolean (true is exist false if not)
-//		   err: error
-
-func EmailExists(db *pgxpool.Pool, email string) (bool, error) {
-
+// Checks if a user exists in the database by an email
+func EmailExists(db *pgxpool.Pool, uid int) (bool, error) {
 	var exists bool
 	err := db.QueryRow(context.Background(),
-		"SELECT EXISTS (SELECT 1 FROM users WHERE email=$1)",
-		email,
+		"SELECT EXISTS (SELECT 1 FROM users WHERE id=$1)",
+		uid,
 	).Scan(&exists)
 	if err != nil {
 		return false, err
@@ -77,45 +73,35 @@ func EmailExists(db *pgxpool.Pool, email string) (bool, error) {
 	return exists, nil
 }
 
-// Purpose: add a new user to the users table
-// Arguments: db: *sql.DB (sql database model),
-//			  uuid: uuid (user id stuff)
-//			  email: string (email of user)
-//			  password: string (hash of password)
-// Return: err: error
-
-func AddNewUser(db *pgxpool.Pool, email, password string) (string, error) {
-	var id string
+// Adds a new user to the users table
+func AddNewUser(db *pgxpool.Pool, email string, password []byte, displayName string) (int, error) {
+	var id int
 	err := db.QueryRow(context.Background(), `
-        INSERT INTO users (email, password, phone, displayName)
-        VALUES ($1, $2, '', '')
+        INSERT INTO users (email, password, displayName)
+        VALUES ($1, $2, $3)
         ON CONFLICT (email) DO NOTHING
         RETURNING id
-    `, email, password).Scan(&id)
+    `, email, password, displayName).Scan(&id)
 
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
-			return "", ErrEmailInUse
+			return -1, ErrEmailInUse
 		}
-		return "", err
+		return -1, err
 	}
 	return id, nil
 }
 
-// Purpose: finds user row by their email
-// Arguments: db: *sql.DB (sql database model),
-//			  email: string (email of user)
-// Return: users: *Users (user data Struct)
-// 		   err: error
+// Finds a user by an email
+func GetByEmail(db *pgxpool.Pool, email string) (*User, error) {
+	var user User
 
-func ExistsByEmail(db *pgxpool.Pool, email string) (*Users, error) {
-
-	var user Users
+	user.Email = email
 
 	err := db.QueryRow(context.Background(),
-		"SELECT uuid, email, password FROM users WHERE email=$1",
+		"SELECT id, password FROM users WHERE email=$1",
 		email,
-	).Scan(&user.ID, &user.Email, &user.Password)
+	).Scan(&user.ID, &user.Password)
 
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
@@ -127,54 +113,36 @@ func ExistsByEmail(db *pgxpool.Pool, email string) (*Users, error) {
 	return &user, nil
 }
 
-// Purpose: hashes users password before storing in db
-// Arguments: password: string (user input password)
-// Return: password_hash: string (hash of password)
-//
-//	err: error
-func HashPassword(password string) (string, error) {
+// Hashes a password using bcrypt
+func HashPassword(password string) ([]byte, error) {
 
 	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return "", err
+		return []byte{}, err
 	}
-	return string(hashed), nil // store this string in your password column
+	return hashed, nil
 }
 
-// Purpose: compares users hash in db to pasword typed in
-// Arguments: password: string (user input password)
-//
-//	hashed_password: string (hash from db)
-//
-// Return: result: (nil == success, nil != failed)
-func CheckPasswordHash(hashedPassword, password string) error {
-
-	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+// Compares users hash in db to typed password
+// Returns nil on success, err on fail
+func CheckPasswordHash(hashedPassword []byte, password string) error {
+	return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
 }
 
-// Purpose: to generate a new pair of access and refresh tokens
-// Arguments: username: string (account username),
-//
-//	userid: string (account id in SQL database)
-//
-// Return: access_token: string (access key to store in browser local storage)
-//
-//	refresh_token: string (this will get stored in the http cookies)
-func generateToken(email string, userid string) (string, string, error) {
+// Generates a new pair of access and refresh tokens. Returns (access_token, refresh_token, error)
+func generateToken(userid int) (string, string, error) {
 
 	creation_time := time.Now()
 
 	access_token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": email,
-		"sub":   userid,
-		"iat":   creation_time.Unix(),
-		"exp":   creation_time.Add(ACCESS_TOKEN_KEEPALIVE).Unix(),
+		"sub": userid,
+		"iat": creation_time.Unix(),
+		"exp": creation_time.Add(ACCESS_TOKEN_KEEPALIVE).Unix(),
 	})
 	refresh_token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": email,
-		"sub":   userid,
-		"iat":   creation_time.Unix(),
-		"exp":   creation_time.Add(REFRESH_TOKEN_KEEPALIVE).Unix(),
+		"sub": userid,
+		"iat": creation_time.Unix(),
+		"exp": creation_time.Add(REFRESH_TOKEN_KEEPALIVE).Unix(),
 	})
 
 	sign_access, err1 := access_token.SignedString([]byte(os.Getenv("access_key")))
@@ -190,11 +158,8 @@ func generateToken(email string, userid string) (string, string, error) {
 	return sign_access, sign_refresh, err2
 }
 
+// Verifies a jwt token
 func verifyToken(tokenString string, secretKey []byte) (jwt.MapClaims, error) {
-	// Purpose: to verfiy jwt tokens
-	// Arguments: tokenString: string (token to verify),
-	// 			  secretKey: string (the key of the token to verify)
-	// Return: if the token is valid this function will return nil
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, jwt.ErrSignatureInvalid
@@ -212,9 +177,9 @@ func verifyToken(tokenString string, secretKey []byte) (jwt.MapClaims, error) {
 	return nil, fmt.Errorf("invalid token")
 }
 
-func RefreshCookieTemplate(c *gin.Context, email string, uid string) (string, error) {
-
-	access, refresh, err := generateToken(email, uid)
+// adds the refresh token to the http cookies and returns the access token
+func RefreshCookieTemplate(c *gin.Context, uid int) (string, error) {
+	access, refresh, err := generateToken(uid)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"detail": "token generation failed"})
 		return "", err
@@ -239,9 +204,8 @@ func RefreshCookieTemplate(c *gin.Context, email string, uid string) (string, er
 //----------------------------------------------------
 //---------------START-OF-API-ENDPOINTS---------------
 
+// Method: POST
 func (s *Server) Refresh(c *gin.Context) {
-	// Method: POST
-
 	jwt_refresh_key := []byte(os.Getenv("refresh_key"))
 
 	refresh_cookie, err := c.Cookie("refresh_token")
@@ -257,24 +221,21 @@ func (s *Server) Refresh(c *gin.Context) {
 		return
 	}
 
-	// DO SQL STUFF HERE YO
+	uid_str, err := token_data.GetSubject()
 
-	email, ok1 := token_data["email"].(string)
-	uid, ok2 := token_data["sub"].(string)
-
-	if !ok1 {
-		fmt.Println(ok1)
-		c.JSON(http.StatusForbidden, gin.H{"detail": "invalid token payload (email)"})
-		return
-	}
-	if !ok2 {
-		c.JSON(http.StatusForbidden, gin.H{"detail": "invalid token payload (sub)"})
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{"detail": "invalid token payload"})
 		return
 	}
 
-	// END OF SQL
+	uid, err := strconv.Atoi(uid_str)
 
-	access, err := RefreshCookieTemplate(c, email, uid)
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{"detail": "invalid token payload"})
+		return
+	}
+
+	access, err := RefreshCookieTemplate(c, uid)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"detail": "token generation failed"})
@@ -284,9 +245,8 @@ func (s *Server) Refresh(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"access_token": access})
 }
 
+// Method: POST
 func (s *Server) Login(c *gin.Context) {
-	// Method: POST
-
 	var login_account struct {
 		Email    string `json:"email" binding:"required"`
 		Password string `json:"password" binding:"required"`
@@ -298,9 +258,7 @@ func (s *Server) Login(c *gin.Context) {
 		return
 	}
 
-	// DO SQL STUFF HERE YO
-
-	user_result, err := ExistsByEmail(s.DB, login_account.Email)
+	user_result, err := GetByEmail(s.DB, login_account.Email)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"detail": "database error"})
 		return
@@ -316,11 +274,7 @@ func (s *Server) Login(c *gin.Context) {
 
 	}
 
-	// END OF SQL
-
-	email := user_result.Email
-	uid := user_result.ID
-	access, err := RefreshCookieTemplate(c, email, uid)
+	access, err := RefreshCookieTemplate(c, user_result.ID)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"detail": "token generation failed"})
@@ -329,11 +283,11 @@ func (s *Server) Login(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"access_token": access,
-		"detail":       "login success",
-		"user":         gin.H{"uuid": uid, "username": login_account.Email},
+		"display_name": user_result.DisplayName,
 	})
 }
 
+// Method: POST
 func (s *Server) Logout(c *gin.Context) {
 	cookie := &http.Cookie{
 		Name:     "refresh_token",
@@ -347,23 +301,20 @@ func (s *Server) Logout(c *gin.Context) {
 
 	http.SetCookie(c.Writer, cookie)
 
-	c.JSON(http.StatusOK, gin.H{
-		"detail": "logout success",
-	})
+	c.Status(http.StatusOK)
 }
 
 func (s *Server) Signup(c *gin.Context) {
 	var register_account struct {
-		Email    string `json:"email" binding:"required"`
-		Password string `json:"password" binding:"required"`
+		Email       string `json:"email" binding:"required"`
+		Password    string `json:"password" binding:"required"`
+		DisplayName string `json:"display_name" binding:"required"`
 	}
 
 	if err := c.ShouldBindJSON(&register_account); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"detail": "Email and password required"})
+		c.JSON(http.StatusBadRequest, gin.H{"detail": "Email, password, and display name required"})
 		return
 	}
-
-	// DO SQL STUFF HERE YO
 
 	email := register_account.Email
 
@@ -375,20 +326,17 @@ func (s *Server) Signup(c *gin.Context) {
 		return
 	}
 
-	uid, err := AddNewUser(s.DB, email, password)
+	uid, err := AddNewUser(s.DB, email, password, register_account.DisplayName)
 	if err != nil {
 		if errors.Is(err, ErrEmailInUse) {
-			c.JSON(http.StatusConflict, gin.H{"detail": "Email address is already in use!"})
+			c.JSON(http.StatusConflict, gin.H{"detail": "Email address is already in use"})
 			return
 		}
 		c.JSON(http.StatusInternalServerError, gin.H{"detail": "database error"})
-		fmt.Println(err)
 		return
 	}
 
-	// END OF SQL
-
-	access, err := RefreshCookieTemplate(c, email, uid)
+	access, err := RefreshCookieTemplate(c, uid)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"detail": "token generation failed"})
@@ -405,7 +353,6 @@ func (s *Server) Signup(c *gin.Context) {
 //----------------------------------------------------
 
 func main() {
-
 	env_err := godotenv.Load()
 	if env_err != nil {
 		log.Fatal("Error loading .env file")
@@ -434,22 +381,8 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
-	// Method: POST
-	// Purpose: to refresh jwt token for http and browser
 	router.POST("/refresh", s.Refresh)
-
-	// Method: POST
-	// Purpose: allow users to create accounts
-	// Arguments:
-	//	username: string,
-	//	password: string
 	router.POST("/signup", s.Signup)
-
-	// Method: POST
-	// Purpose: users can login to their accounts
-	// Arguments:
-	//	username: string,
-	//	password: string
 	router.POST("/login", s.Login)
 
 	router.Run("localhost:8080")
