064096af7370274c870486703555013980b3dbac
Riley Motylinski <riley@motylinski.com>
2025-12-06 19:09:23 -0600

    fix: ran linter

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,25 +1,25 @@
-import { FC } from "react";
 import { GithubLink } from "./GithubLink";
 
+import type { FC } from "react";
 
 export const Footer: FC = () => {
-    return <>
-        <div className="grid grid-cols-3 justify-center p-2 border-t border-gray-200 bg-black/30 py-4 text-center text-sm text-gray-200 content-center">
-            
-            <><p></p></>
+  return (
+    <>
+      <div className="grid grid-cols-3 content-center justify-center border-t border-gray-200 bg-black/30 p-2 py-4 text-center text-sm text-gray-200">
+        <>
+          <p></p>
+        </>
 
-            <footer className="relative z-10">
-            
-                <p>© {new Date().getFullYear()} Gopher Grub · University of Minnesota</p>
-        
-            </footer>
-        
-          
-            <div className="self-center justify-self-end mr-5">
-                <GithubLink />
-            </div>
-        
+        <footer className="relative z-10">
+          <p>
+            © {new Date().getFullYear()} Gopher Grub · University of Minnesota
+          </p>
+        </footer>
+
+        <div className="mr-5 self-center justify-self-end">
+          <GithubLink />
         </div>
+      </div>
     </>
-    
-}
+  );
+};
--- frontend/src/components/GithubLink.tsx
+++ frontend/src/components/GithubLink.tsx
@@ -1,17 +1,21 @@
-import { FC } from "react";
 import Image from "next/image";
 
-import GithubLogo from "../components/res/github-mark-white.svg";
+import GithubLogo from "./res/github-mark-white.svg";
+
+import type { FC } from "react";
 
 export const GithubLink: FC = () => {
-   return (
-            <a href="https://github.com/david-callender/FoodFinder"><Image
-            src={GithubLogo}
-            width={20}
-            height={20}
-            alt="Gopher Grub Logo"
-            className="ml-50"
-            />
-        </a>
-    )
-}
+  return (
+    <>
+      <a href="https://github.com/david-callender/FoodFinder">
+        <Image
+          src={GithubLogo}
+          width={20}
+          height={20}
+          alt="Gopher Grub Logo"
+          className="ml-50"
+        />
+      </a>
+    </>
+  );
+};
